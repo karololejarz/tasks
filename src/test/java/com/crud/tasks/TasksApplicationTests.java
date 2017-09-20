d89c5858593dd2a6f935d8e64fda14b9dcca6029
karololejarz <karol1olejarz@gmail.com>
2017-09-20 11:49:20 +0200

    Moduł 15.

--- src/test/java/com/crud/tasks/TasksApplicationTests.java
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -1,10 +1,15 @@
 package com.crud.tasks;
 
+import com.crud.tasks.controller.TaskController;
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,21 +17,32 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class TasksApplicationTests {
 
+	@Autowired
+	TaskController controller;
+
 	@Test
 	public void contextLoads() {
 	}
 
 	@Test
-	public void findTaskByIdAndDeleteTaskById() {
-		DbService service = new DbService();
-		Task task99 = new Task(Long.valueOf(99),"test99","test99");
-		service.saveTask(task99);
+	public void findTaskByIdAndDeleteTaskById() throws TaskNotFoundException {
 
-		Assert.assertEquals("test99",service.getTask(task99.getId()).get().getTitle());
+		Task task1 = new Task(Long.valueOf(1),"test1","test1");
+		Task task2 = new Task(Long.valueOf(2),"test2","test2");
+		TaskDto dto1 = new TaskDto(task1.getId(),task1.getTitle(),task1.getContent());
+		TaskDto dto2 = new TaskDto(task2.getId(),task2.getTitle(),task2.getContent());
 
-		service.deleteTask(task99.getId());
+		controller.createTask(dto1);
+		controller.createTask(dto2);
 
-		Assert.assertEquals(false,service.getAllTasks().contains(task99));
+		Assert.assertEquals("test1",controller.getTask(task1.getId()).getTitle());
+		Assert.assertEquals("test2",controller.getTask(task2.getId()).getTitle());
+
+		controller.deleteTask(task1.getId());
+		controller.deleteTask(task2.getId());
+
+		Assert.assertEquals(false,controller.getTasks().contains(task1));
+		Assert.assertEquals(false,controller.getTasks().contains(task2));
 	}
 
 }
